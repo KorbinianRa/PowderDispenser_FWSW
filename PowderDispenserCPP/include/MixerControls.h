49780f6589efae9933e66e0ad95de77c2f47657f
Korbinian <s230699@dtu.dk>
2025-04-04 10:42:36 +0200

    Cleaned up conflict markers in MixerControls and README

--- PowderDispenserCPP/include/MixerControls.h
+++ PowderDispenserCPP/include/MixerControls.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MIXERCONTROLS_H
 #define MIXERCONTROLS_H
 
@@ -31,37 +30,3 @@ private:
 
 #endif // MIXERCONTROLS_H
 
-=======
-#ifndef MIXERCONTROLS_H
-#define MIXERCONTROLS_H
-
-#include "Utils.h"
-#include <SparkFun_Qwiic_Relay.h>
-
-// Define relay addresses
-#define RELAY_ADDR1 0x18
-#define RELAY_ADDR2 0x19
-
-class MixerControls {
-public:
-    MixerControls(Utils& utils);
-
-    void setupRelay(Qwiic_Relay &relay);
-    bool checkRelayState(Qwiic_Relay &relay);
-    void run(Qwiic_Relay &relay, float runTime);
-
-    Qwiic_Relay& getMixerRelay() {return relay_mixer;}
-    Qwiic_Relay& getDrainRelay() {return relay_drain;} 
-
-    void setupPump(uint8_t pin);
-    void runPump(uint8_t pin, float runTime);
-
-private:
-    Utils& utils;
-    Qwiic_Relay relay_mixer;
-    Qwiic_Relay relay_drain;
-};
-
-#endif // MIXERCONTROLS_H
-
->>>>>>> c5e728c45412297ccfbd7313ba623480bfd3dee3
